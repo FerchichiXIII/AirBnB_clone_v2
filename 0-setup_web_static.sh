02eb67f4574c3ad6a5231db22d2f4beed077e1cb
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-27 21:26:34 +0100

    update task0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,4 +8,4 @@ echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "38i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo service nginx start
