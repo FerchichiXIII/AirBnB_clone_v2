bcf04811bc7ceb2ec68e17551ea10c235a9be8e0
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-27 22:15:52 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,18 +7,26 @@ from datetime import datetime
 import os
 
 
+env.usr = ['35.173.249.36', '3.95.204.110']
+
+
 def do_deploy(archive_path):
+    """
+    Distributes an archive to the web servers
+    """
     if not os.path.exists(archive_path):
         return False
-    else:
+    try:
+        file_n = archive_path.split("/")[-1]
+        path = archive_path.split(file_n)[0]
         put(archive_path, "/tmp/")
-        file = archive_path.split("/")[-1]
-        archive = file.split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(archive))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, archive))
-        run("rm /tmp/{}".format(file))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(archive, archive))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(archive))
+        run("mkdir -p /data/web_static/releases/{}".format(path))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(file_n, path))
+        run("rm /tmp/{}".format(file_n))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(date, date))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(path))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive))
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(path))
         return True
+    except:
+        return False
