a3724993726d2cdee9eb5707164cca7584158cfa
FerchichiXIII <anasferchichi22@gmail.com>
2022-09-03 15:13:37 +0100

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo "<h1>Hello from Nginx!</h1>" | sudo tee /data/web_static/releases/test/index.html
+echo "<h1>Holberton School</h1>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown ubuntu:ubuntu -hR /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
