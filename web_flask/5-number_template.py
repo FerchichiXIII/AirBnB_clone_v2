a9713345bc0b93ff595ef41d9d967209a236394f
FerchichiXIII <anasferchichi22@gmail.com>
2022-09-10 15:07:45 +0100

    fix h-numbeer

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """ Hello HBNB """
-    return render_template('5-number_template.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if "__main__" == __name__:
