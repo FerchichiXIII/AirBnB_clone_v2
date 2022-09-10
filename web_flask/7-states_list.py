281088f4e46660846ed08903199df4d8c538adde
FerchichiXIII <anasferchichi22@gmail.com>
2022-09-10 16:50:33 +0100

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+	Write a script that starts a Flask web application
+"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+	"""
+	Return a HTML page with all states
+	"""
+	states = storage.all("State").values()
+	return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown(ecxt):
+	storage.close()
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
