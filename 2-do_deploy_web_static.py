1d3dc01c084a92a9c9ea842adfd83ecfde2a31ae
FerchichiXIII <anasferchichi22@gmail.com>
2022-09-10 11:45:12 +0100

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,25 +11,19 @@ env.usr = ['35.173.249.36', '54.173.105.242']
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers
-    """
-    if exists(archive_path) is False:
-        return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/{}".format(no_ext)
-        run("mkdir -p {}".format(path))
-        put(archive_path, path)
-        with cd(path):
-            run("tar -xzf {}".format(file_n))
-            run("rm {}".format(file_n))
-            run("mv {} {}".format(no_ext, "current"))
-            run("rm -rf releases/*")
-            run("ln -s {} releases/current")
-            run("service nginx restart")
-            return True
-    except Exception as e:
-        print(e)
-        return False
+    """ this is a usles comment """
+    if exists(archive_path):
+        s = archive_path.split("/")[-1]
+        se = s.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, se))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(s, path, se))
+        run('rm /tmp/{}'.format(s))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, se))
+        run('rm -rf {}{}/web_static'.format(path, se))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, se))
+        return True
+
+    return False
